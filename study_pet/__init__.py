d78bb984f788e1da89d21fad437d2ea14a043eff
JunHaoChen16 <jc12230@nyu.edu>
2025-11-02 21:09:22 -0500

    export functions in init.py

--- study_pet/__init__.py
+++ study_pet/__init__.py
@@ -1,2 +1,11 @@
-def hello():
-    return "🐾 Hello from StudyPet!"
+from .tracker import start_session, end_session
+from .pet.core import get_status
+from .data_manager import reset_state as _reset_state
+
+__all__ = ["start_session", "end_session", "get_status", "reset_pet"]
+
+
+def reset_pet():
+    """Resets all pet data and progress."""
+    _reset_state()
+    print("Your pet has been reborn! All progress reset.")
